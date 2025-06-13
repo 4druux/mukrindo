832d8ac7884feb67507f3f254decde51e440300a
andrewsamagachandra <andrewsamagachandra@gmail.com>
2025-06-13 18:19:42 +0700

    update backend add api in callback url /auth/google/callback (main)

--- backend/config/passport-setup.js
+++ backend/config/passport-setup.js
@@ -22,7 +22,7 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/auth/google/callback",
+        callbackURL: "api/auth/google/callback",
         proxy: true,
       },
       async (accessToken, refreshToken, profile, done) => {
